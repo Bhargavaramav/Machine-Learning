6eecbeaa01350bb9184f7f744598d3e0fc9ffc06
Bhargavarama vadlamudi <bxv27820@ucmo.edu>
2024-07-01 03:08:46 +0000

    ds

--- models/Marts/LatestReviews_per_listings.sql
+++ models/Marts/LatestReviews_per_listings.sql
@@ -1,9 +1,19 @@
+
+{{ config(materialized='table')}}
+
+{{ config(
+    pre_hook="truncate  LatestReviews_per_listings"   
+) }}
+
+
+
+
 with cte as (select * from airbnb.raw.raw_listings a
 join airbnb.raw.raw_reviews b
 on a.id=b.listing_id
 )
-
-select listing_id, max(date) as latest_review_date,
-reviewer_name,comments
-from cte
-group by listing_id,reviewer_name,comments
+select 1
+-- select listing_id, max(date) as latest_review_date,
+-- reviewer_name,comments
+-- from cte
+--group by listing_id,reviewer_name,comments
